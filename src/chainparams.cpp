7a3464ae98550278a3aa80799fab624ca20867ad
Kolby Moroz <31669092+mrmetech@users.noreply.github.com>
2018-12-26 16:03:15 -0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -141,8 +141,8 @@ public:
         consensus.nMasternodeMinimumConfirmations = 15;
         consensus.BIP34Height = 0;
         consensus.BIP34Hash = uint256S("0x000001f35e70f7c5705f64c6c5cc3dea9449e74d5b5c7cf74dad1bcca14a8012");
-        consensus.BIP65Height = 619382; // 00000000000076d8fcea02ec0963de4abfd01e771fec0863f960c2c64fe6f357
-        consensus.BIP66Height = 245817; // 00000000000b1fa2dfa312863570e13fae9ca7b5566cb27e55422620b469aefa
+        consensus.BIP65Height = 0; // 
+        consensus.BIP66Height = 0; // this should be fine it is hard to tell when they were accepted by the network bip34 was added at block 0 but getblockchaininfo says found at 1000 this shouldn't affect much and it should be assumed this was added at the start of the chain
         consensus.DIP0001Height = 12096;
         consensus.powLimit = uint256S("00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"); // ~uint256(0) >> 20
         consensus.nPowTargetTimespan = 14 * 24 * 60 * 60; // two weeks
@@ -187,37 +187,41 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xbf;
-        pchMessageStart[1] = 0x0c;
-        pchMessageStart[2] = 0x6b;
-        pchMessageStart[3] = 0xbd;
-        vAlertPubKey = ParseHex("048240a8748a80a286b270ba126705ced4f2ce5a7847b3610ea3c06513150dade2a8512ed5ea86320824683fc0818f0ac019214973e677acd1244f6d0571fc5103");
-        nDefaultPort = 9999;
+        pchMessageStart[0] = 0x48;
+        pchMessageStart[1] = 0xb3;
+        pchMessageStart[2] = 0xf2;
+        pchMessageStart[3] = 0x21;
+        vAlertPubKey = ParseHex("0428afa3a0023803d312ef8082b1687bb79e54ca05b77b0c14d10158e760d2b8a186b590bc2041fe135d18e5f89274580d9e18d5275f9dc00a5f3c2181eec14b70");
+        nDefaultPort = 26285;
         nPruneAfterHeight = 100000;
 
-        genesis = CreateGenesisBlock(1390095618, 28917698, 0x1e0ffff0, 1, 50 * COIN);
+        genesis = CreateGenesisBlock(1516830940, 531473, 0x1e0ffff0, 1, 50 * COIN);
         consensus.hashGenesisBlock = genesis.GetHash();
-        assert(consensus.hashGenesisBlock == uint256S("0x00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6"));
-        assert(genesis.hashMerkleRoot == uint256S("0xe0028eb9648db56b1ac77cf090b99048a8007e2bb64b68f092c03c7f56a662c7"));
+        assert(consensus.hashGenesisBlock == uint256S("0x000009110a70cd2bf2cdcae9a8b1425bb074c7b7b08570c2c9f04fe8668c6589"));
+        assert(genesis.hashMerkleRoot == uint256S("0x07aed85c2fb7ad6855aca4a9822fe50c112811be3bfc79114beb0145a80b90f1"));
 
 
-        vSeeds.push_back(CDNSSeedData("dash.org", "dnsseed.dash.org"));
-        vSeeds.push_back(CDNSSeedData("dashdot.io", "dnsseed.dashdot.io"));
-        vSeeds.push_back(CDNSSeedData("masternode.io", "dnsseed.masternode.io"));
-        vSeeds.push_back(CDNSSeedData("dashpay.io", "dnsseed.dashpay.io"));
+        vSeeds.push_back(CDNSSeedData("dinerocoin.org", "seed1.dinerocoin.org"));
+        vSeeds.push_back(CDNSSeedData("dinerocoin.org", "seed2.dinerocoin.org"));
+        vSeeds.push_back(CDNSSeedData("dinerocoin.org", "seed3.dinerocoin.org"));
+        vSeeds.push_back(CDNSSeedData("dinerocoin.org", "seed4.dinerocoin.org"));
+        vSeeds.push_back(CDNSSeedData("dinerocoin.org", "seed5.dinerocoin.org"));
+        vSeeds.push_back(CDNSSeedData("dinerocoin.org", "seed6.dinerocoin.org"));
+        vSeeds.push_back(CDNSSeedData("dinerocoin.org", "seed7.dinerocoin.org"));
+        vSeeds.push_back(CDNSSeedData("dinerocoin.org", "seed8.dinerocoin.org"));
 
-        // Dash addresses start with 'X'
-        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,76);
-        // Dash script addresses start with '7'
-        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,16);
-        // Dash private keys start with '7' or 'X'
+        // Dinero addresses start with 'D'
+        base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,30);
+        // Dinero script addresses start with '7'
+        base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,13);
+        // Dinero private keys start with '7' or 'X'
         base58Prefixes[SECRET_KEY] =     std::vector<unsigned char>(1,204);
-        // Dash BIP32 pubkeys start with 'xpub' (Bitcoin defaults)
+        // Dinero BIP32 pubkeys start with 'xpub' (Bitcoin defaults)
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
-        // Dash BIP32 prvkeys start with 'xprv' (Bitcoin defaults)
+        // Dinero BIP32 prvkeys start with 'xprv' (Bitcoin defaults)
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        // Dash BIP44 coin type is '5'
+        // Dinero BIP44 coin type is '5'
         nExtCoinType = 5;
 
         vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
@@ -232,39 +236,36 @@ public:
         nPoolMaxTransactions = 3;
         nFulfilledRequestExpireTime = 60*60; // fulfilled requests expire in 1 hour
 
-        strSporkAddress = "Xgtyuk76vhuFW2iT7UAiHgNdWXCf3J34wh";
+        strSporkAddress = "D94pyC6TgYH3vhDHc6sHNUrLVTPZ17Jtdy";
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-            (  1500, uint256S("0x000000aaf0300f59f49bc3e970bad15c11f961fe2347accffff19d96ec9778e3"))
-            (  4991, uint256S("0x000000003b01809551952460744d5dbb8fcbd6cbae3c220267bf7fa43f837367"))
-            (  9918, uint256S("0x00000000213e229f332c0ffbe34defdaa9e74de87f2d8d1f01af8d121c3c170b"))
-            ( 16912, uint256S("0x00000000075c0d10371d55a60634da70f197548dbbfa4123e12abfcbc5738af9"))
-            ( 23912, uint256S("0x0000000000335eac6703f3b1732ec8b2f89c3ba3a7889e5767b090556bb9a276"))
-            ( 35457, uint256S("0x0000000000b0ae211be59b048df14820475ad0dd53b9ff83b010f71a77342d9f"))
-            ( 45479, uint256S("0x000000000063d411655d590590e16960f15ceea4257122ac430c6fbe39fbf02d"))
-            ( 55895, uint256S("0x0000000000ae4c53a43639a4ca027282f69da9c67ba951768a20415b6439a2d7"))
-            ( 68899, uint256S("0x0000000000194ab4d3d9eeb1f2f792f21bb39ff767cb547fe977640f969d77b7"))
-            ( 74619, uint256S("0x000000000011d28f38f05d01650a502cc3f4d0e793fbc26e2a2ca71f07dc3842"))
-            ( 75095, uint256S("0x0000000000193d12f6ad352a9996ee58ef8bdc4946818a5fec5ce99c11b87f0d"))
-            ( 88805, uint256S("0x00000000001392f1652e9bf45cd8bc79dc60fe935277cd11538565b4a94fa85f"))
-            ( 107996, uint256S("0x00000000000a23840ac16115407488267aa3da2b9bc843e301185b7d17e4dc40"))
-            ( 137993, uint256S("0x00000000000cf69ce152b1bffdeddc59188d7a80879210d6e5c9503011929c3c"))
-            ( 167996, uint256S("0x000000000009486020a80f7f2cc065342b0c2fb59af5e090cd813dba68ab0fed"))
-            ( 207992, uint256S("0x00000000000d85c22be098f74576ef00b7aa00c05777e966aff68a270f1e01a5"))
-            ( 312645, uint256S("0x0000000000059dcb71ad35a9e40526c44e7aae6c99169a9e7017b7d84b1c2daf"))
-            ( 407452, uint256S("0x000000000003c6a87e73623b9d70af7cd908ae22fee466063e4ffc20be1d2dbc"))
-            ( 523412, uint256S("0x000000000000e54f036576a10597e0e42cc22a5159ce572f999c33975e121d4d"))
-            ( 523930, uint256S("0x0000000000000bccdb11c2b1cfb0ecab452abf267d89b7f46eaf2d54ce6e652c"))
-            ( 750000, uint256S("0x00000000000000b4181bbbdddbae464ce11fede5d0292fb63fdede1e7c8ab21c"))
-            ( 888900, uint256S("0x0000000000000026c29d576073ab51ebd1d3c938de02e9a44c7ee9e16f82db28"))
+            (  0, uint256S("0x000009110a70cd2bf2cdcae9a8b1425bb074c7b7b08570c2c9f04fe8668c6589"))
+            (  10, uint256S("0x00000de65209026fb9bded32f3204fd5447acf8be7060d5db1c9da1a29c8f70b"))
+            (  100, uint256S("0x00000e710497aa6e44457ee891bbbc09c6bafdaeeec1a22ceb771a36373951a9"))
+            (  1000, uint256S("0x0000000000aaa395a3fe7321eea5a8845cb45ef7f0679c43a7b64a245a5f1147"))
+            (  2000, uint256S("0x000000000161587975fde7423c09753814d00594df0a9121582aeb51f4e6e00f"))
+            (  3000, uint256S("0x0000000000e8694a2f9f2d6aec36dabf1aa2b14a65fc0cc306196698a3d0c7d3"))
+            (  3323, uint256S("0x0000000001c0168cfcc68ee07b6187da4ac859570cb02657b991f29c44ea8a8d"))
+            (  3500, uint256S("0x000000000cc352fb29c93dbbebdd6a779faf240624ace8cb618c87ff2afc9256"))
+            (  3874, uint256S("0x0000000003982b3b056f44c9782d841742d9992ab3f95049fd87653802d8d7d8"))
+            (  3900, uint256S("0x0000000003e37467dc299568cade2e27bfd777c4f23bd2fa7b256b2e6fa360a4"))
+            (  4046, uint256S("0x00000000027d296226b0ee7e35ad4e7c3eaafe2bff475bc59fddb80852cdc8a4"))
+            (  5745, uint256S("0x000000000056b16f8b464f235bca3ec4455674ba73cb95538f5318b141e393d4"))
+            (  8000, uint256S("0x0000000000d4a3199abcdf33cbaa6c1af2e145bda07ebdf29b070d0265cdc120"))
+            (  11500, uint256S("0x000000000074a6869637495962c1a37e0a2c20bb00bf1384227a0dc715b0813c"))
+            (  12350, uint256S("0x0000000000882760711b236c7ccdbf033b57e0b27b3dea10eb6fe4556cf63d31"))
+            (  13400, uint256S("0x0000000001195f13e59033f4ee78521254bef3e3bf6a053a5662edba39c26632"))
+            (  18800, uint256S("0x0000000000680ef8b1222f64263e04a1c92c1bc7abf6d2c8e11e5325811288cf"))
+            (  35000, uint256S("0x00000000063721e5ee07a09fe6fd73e76b4201728260dad77b5a8c4b8828c786"))
+            (  110000, uint256S("0x000000000b9279705a5dce77ff833da59ea84a0af235a76f1c4a2dcd972177f2"))
         };
 
         chainTxData = ChainTxData{
-            1529305236, // * UNIX timestamp of last known number of transactions
-            6155435,    // * total number of transactions between genesis and that timestamp
+            1527371364, // * UNIX timestamp of last checkpoint block
+            110000,    // * total number of transactions between genesis and last checkpoint
                         //   (the tx=... number in the SetBestChain debug.log lines)
-            0.1         // * estimated number of transactions per second after that timestamp
+            2200        // * estimated number of transactions per day after checkpoint
         };
     }
 };
